0244931cfeaa4cd8bd80cc72496fcc944d2a1b85
Tinatin Rodinadze <rodinadze02@gmail.com>
2024-04-05 15:40:11 +0400

    syncing

--- HotelProject.Web/Controllers/ManagersController.cs
+++ HotelProject.Web/Controllers/ManagersController.cs
@@ -1,4 +1,5 @@
-﻿using HotelProject.Repository;
+﻿using HotelProject.Models;
+using HotelProject.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.Web.Controllers
@@ -6,15 +7,46 @@ namespace HotelProject.Web.Controllers
     public class ManagersController : Controller
     {
         private readonly ManagerRepository _managerRepository;
-        public ManagersController()
+       
+        public ManagersController(ManagerRepository managerRepository)
         {
-            _managerRepository = new ManagerRepository();
+            _managerRepository = managerRepository;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var result = await _managerRepository.GetManagers();
             return View(result);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Manager model)
+        {
+            await _managerRepository.AddManager(model);
+            return RedirectToAction("Index");
+        }
+
+        /*
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _managerRepository.GetSingleHotel(id);
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeletePOST(int id)
+        {
+            await _managerRepository.DeleteManager(id);
+            return RedirectToAction("Index");
+        }
+        */
     }
 }
--- HotelProject.Web/Controllers/RoomsController.cs
+++ HotelProject.Web/Controllers/RoomsController.cs
@@ -1,4 +1,5 @@
-﻿using HotelProject.Repository;
+﻿using HotelProject.Models;
+using HotelProject.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.Web.Controllers
@@ -6,15 +7,28 @@ namespace HotelProject.Web.Controllers
     public class RoomsController : Controller
     {
         private readonly RoomRepository _roomRepository;
-        public RoomsController()
+       
+        public RoomsController(RoomRepository roomRepository)
         {
-            _roomRepository = new RoomRepository();
+            _roomRepository = roomRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             var result = await _roomRepository.GetRooms();
             return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Room model)
+        {
+            await _roomRepository.AddRoom(model);
+            return RedirectToAction("Index");
         }
     }
 }
